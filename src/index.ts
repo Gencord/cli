d2900ddcfe6e5b1b63e790c577b82348189ff04e
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2021-03-01 20:39:25 -0800

    chore(CLI): some things

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-const myArgs = process.argv.slice(2);
+import { table } from "table";
 
-switch (myArgs[0]) {
+const args = process.argv.slice(2);
+
+switch (args[0]) {
   case "--help":
-    console.log("help command");
+    let data, output;
+
+    data = [
+      ["0A", "0B", "0C"],
+      ["1A", "1B", "1C"],
+      ["2A", "2B", "2C"],
+    ];
+
+    output = table(data);
+
+    console.log(output);
+
     break;
   default:
     console.log("Sorry, that is not something I know how to do.");
